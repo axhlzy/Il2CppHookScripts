4c3e9b99e838ddcb6cdb3e8e8fae5ab1b3f3eba1
唱过阡陌 <20512058+axhlzy@users.noreply.github.com>
2020-08-13 17:50:16 +0800

    Update fts.js

--- FTS/fts.js
+++ FTS/fts.js
@@ -1,3 +1,18 @@
+   function anti_fgets() {
+        show_log("anti_fgets");
+        var fgetsPtr = Module.findExportByName("libc.so", "fgets");
+        var fgets = new NativeFunction(fgetsPtr, 'pointer', ['pointer', 'int', 'pointer']);
+        Interceptor.replace(fgetsPtr, new NativeCallback(function (buffer, size, fp) {
+            var retval = fgets(buffer, size, fp);
+            var bufstr = Memory.readUtf8String(buffer);
+            if (bufstr.indexOf("TracerPid:") > -1) {
+                Memory.writeUtf8String(buffer, "TracerPid:\t0");
+                // dmLogout("tracerpid replaced: " + Memory.readUtf8String(buffer));
+            }
+            return retval;
+        }, 'pointer', ['pointer', 'int', 'pointer']));
+    };
+
 function CheckPoen(){
 
     var popen = Module.findExportByName("libc.so","popen")
