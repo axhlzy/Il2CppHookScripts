96d0ffceb98d1e2d7f8eb70e977c9e7ac72b5854
约你一起偷西瓜 <20512058+axhlzy@users.noreply.github.com>
2020-09-09 08:22:24 +0800

    Update bpoints.js

--- bpoints.js
+++ bpoints.js
@@ -1,10 +1,10 @@
-
 // 结合Il2CppDumper使用，用于批量快速下断点，跟踪native函数调用
 // frida -U -f <PackageName> -l C:\Users\lzy\utils\bpoints.js --no-pause
 
 
 const soName = "libil2cpp.so"
 
+//使用的时候使用dsp.py生成的两个地址名称替换调这里的两个即可
 const arrayAddr =
     ['0x71541c', '0x715b38', '0x715be4', '0x715c61']
 
@@ -74,4 +74,44 @@ function hook_dlopen() {
     }
 }
 
-setImmediate(hook_dlopen())
+setImmediate(hookJava(),hook_dlopen())
+
+function hookJava(){
+    //java 方法的 hook
+    Java.perform(function(){
+        Java.use("xxxxxxx").Testfunc.implementation = function(){
+            return this.Testfunc.apply(this,arguments)
+        }
+    })
+    //java实例的查找
+    Java.choose("xxxxx",{
+        onMatch:function(obj){
+
+        },
+        onComplete:function(){
+
+        }
+    })
+}
+
+function hookNative(){
+
+    var addr = Module.findBaseAddress(soName)
+    //方法调用
+    var func = new NativeFunction(addr.add(0x123),'void','pointer')
+    func(ptr(0))
+    //方法拦截
+    Interceptor.attach(addr.add(0x123),{
+        onEnter:function(args){
+
+        },
+        onLeave:function(retval){
+
+        }
+    })
+    //方法替换
+    Interceptor.replace(func,new NativeCallback(function(arg){
+        
+    },'void',['pointer','pointer']))
+
+}
