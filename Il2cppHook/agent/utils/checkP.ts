a79522f5da22868df394813655eb36fe952b5890
axhlzy <axhlzy@live.cn>
2022-06-28 10:27:21 +0800

    checkPointer update

--- Il2cppHook/agent/utils/checkP.ts
+++ Il2cppHook/agent/utils/checkP.ts
@@ -6,12 +6,69 @@ import { TYPE_CHECK_POINTER } from "../base/globle"
  * @param {Pointer} value
  * @returns ptr
  */
-const checkPointer = (value: TYPE_CHECK_POINTER, throwErr: boolean = false, showLog: boolean = false): NativePointer | NativePointerValue => {
-    // if (typeof value == "number") value = ptr(value)
-    // if (typeof value != "string" && !(value instanceof Array)) return ptr(0)
-    // if (typeof value == "function") return value as NativePointer
+const checkPointer = (value: TYPE_CHECK_POINTER, throwErr: boolean = false, showLog: boolean = false): NativePointer => {
+    if (typeof value === "number") {
+        return calPointer(new NativePointer(value))
+    } else if (typeof value === "string") {
+        return Module.findExportByName(null, value) as NativePointer
+    } else if (typeof value === "object") {
+        if (value instanceof NativePointer) {
+            return calPointer(value)
+        } else if (value instanceof Array<string | number>) {
+            if (!checkValue(value as Array<string | number>)) {
+                if (throwErr) throw new Error("checkPointer: checkValue Error")
+                else return ptr(0)
+            }
+            switch (value.length) {
+                case 1:
+                    return Module.findExportByName(null, value[0] as string) as NativePointer
+                case 2:
+                    return Module.findExportByName(value[0] as string, value[1] as string) as NativePointer
+                case 3:
+                    return find_method(value[0] as string, value[1] as string, value[2] as string, value[3] as number)
+                default:
+                    if (throwErr) throw new Error("checkPointer:UnKnow value length \nArray<> length must be 1,2,3")
+                    else return ptr(0)
+            }
+        } else {
+            if (throwErr) throw new Error("checkPointer: Error type")
+            else return ptr(0)
+        }
+    }
+    return ptr(0)
 
-    return Il2Cpp.module.base.add(ptr(value as unknown as number))
+    function calPointer(mPtr: NativePointer): NativePointer {
+        if (mPtr.isNull() || mPtr.compare(soAddr)) return mPtr
+        try {
+            let tmpValue = Process.findModuleByAddress(mPtr)
+            if (tmpValue === null) {
+                let addValue = Il2Cpp.module.base.add(mPtr)
+                let tmpModule = Process.findModuleByAddress(addValue)
+                if (tmpModule === null) {
+                    if (throwErr) throw new Error("checkPointer: can't find module")
+                    else return ptr(0)
+                }
+                else return addValue
+            } else return mPtr
+        } catch (error) {
+            if (throwErr) throw error
+            return ptr(0)
+        }
+    }
+
+    function checkValue(value: Array<number | string>): boolean {
+        if (value.length == 3) {
+            if (typeof value[0] !== "string") return false
+            if (typeof value[1] !== "string") return false
+            if (typeof value[2] !== "number") return false
+        }
+        for (let i = 0; i < value.length; i++) {
+            if (value.length != 3) {
+                if (typeof value[i] !== "string") return false
+            }
+        }
+        return true
+    }
 }
 
 declare global {
