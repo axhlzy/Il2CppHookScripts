c620ae2e1d3a0b7f35799330f0fef719c6a41416
axhlzy <axhlzy@live.cn>
2022-07-07 16:09:52 +0800

    add classUtils

--- Il2cppHook/agent/java/classUtils.ts
+++ Il2cppHook/agent/java/classUtils.ts
@@ -19,6 +19,46 @@ const findJavaClass = (className: string = "com.unity3d.player.UnityPlayerActivi
     })
 }
 
+const showAllClassesMethods = () => {
+    Java.perform(function () {
+        Java.enumerateLoadedClasses({
+            onMatch: function (className) {
+                LOG("[*] Class Name: " + className);
+                var db1 = Java.use(className);
+                var methodArr = db1.class.getMethods();
+                for (var m in methodArr) {
+                    LOG("\t" + methodArr[m]);
+                }
+            },
+            onComplete: function () { }
+        });
+    });
+}
+
+const showAllClasses = () => {
+    Java.perform(function () {
+        Java.enumerateLoadedClasses({
+            onMatch: function (className) {
+                LOG(className);
+            },
+            onComplete: function () { }
+        });
+    });
+}
+
+const showSpecificClassMethods = () => {
+    Java.perform(function () {
+        var class_name = "android.security.keystore.KeyGenParameterSpec$Builder";
+        var db1 = Java.use(class_name);
+        var methodArr = db1.class.getMethods();
+        LOG("[*] Class Name: " + class_name)
+        LOG("[*] Method Names:")
+        for (var m in methodArr) {
+            LOG(methodArr[m]);
+        }
+    });
+}
+
 export { }
 
 declare global {
